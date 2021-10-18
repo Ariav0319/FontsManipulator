d8940f625b679c1b043d736c57935b21a2b2a180
Aria0319 <72637267+Ariav0319@users.noreply.github.com>
2021-10-17 20:01:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 115);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+function draw(){
+    background('#969A97');
+    textSize(leftWristX, rightWristX, difference);
+    fill('#182aa1');
+    text('Aria', 50, 400,);
+}
